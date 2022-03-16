6058327e65e77d90ee1ff34b4a1f66a4f31efe46
Priyank <priyankdhameliya56@gmail.com>
2022-03-16 15:27:10 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <p>sdsdsdsd!!!!!!!</p>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
